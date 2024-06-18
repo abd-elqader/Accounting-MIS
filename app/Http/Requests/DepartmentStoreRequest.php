7181171991b9595eeea923be98196fb964c5471d
pro-mohamed-hamada <pro.mohamed.hamada@gmail.com>
2024-06-18 22:35:35 +0200

    bug fix department store request

--- app/Http/Requests/DepartmentStoreRequest.php
+++ app/Http/Requests/DepartmentStoreRequest.php
@@ -22,7 +22,7 @@ class DepartmentStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|unique:currencies,name',
+            'name' => 'required|string',
         ];
     }
 
